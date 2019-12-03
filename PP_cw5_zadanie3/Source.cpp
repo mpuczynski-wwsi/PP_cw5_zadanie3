59a3df4bebdc402ff7cd23a33b3bb81b450053d2
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-12-03 07:49:59 +0100

    init

---
+++ PP_cw5_zadanie3/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include<cstdio>
+#include <iostream>
+
+using namespace std;
+
+/*
+Dana jest następująca funkcja rekurencyjna:
+int coztojest(int a, int b){
+if(b == 0)
+	return 0;
+if(b % 2 == 0)
+	return coztojest(a+a, b/2);
+return coztojest(a+a, b/2) +a;
+}
+
+a) Jakie będą wyniki wywołań coztojest(2, 25) i  coztojest(3, 11) ?
+b) Co oblicza funkcja coztojest(a, b), zakładając, że  a i b są liczbami całkowitymi dodatnimi?
+c) Co będzie obliczać funkcja coztojest(a, b) (zakładając, że  a i b są liczbami całkowitymi dodatnimi) , gdy znak + zostanie zastąpiony przez *, a instrukcja return 0 przez instrukcję return 1?
+
+*/
+
+int main() {
+
+}
